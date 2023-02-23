a30f97c5d40a1a5c9477084f1c49d02068c78b19
SimpPoseidon <mehrunkart@gmail.com>
2023-02-23 14:43:08 +0530

    tatta

--- issue.php
+++ issue.php
@@ -1,96 +1,181 @@
 <?php
 include "dbconnect.php";
 
-$doi = date("Y/m/d");
 date_default_timezone_set("Asia/Kolkata");
+$doi = date("Y/m/d");
 $Available=false;
 $sql_m;
 $BookNoIssue=0;
 $b=$_POST["Book_No"];
 $m=$_POST["Member_ID"];
 $issueBy= $_POST["issuedBy"];
-$sql_b="SELECT * from books where Book_No = $b";
-if($issueBy =="Student"){
-    $sql_m="SELECT * from student where Member_ID = '$m'";
+$sql_b="SELECT * from books;";
+
+$sql_check="SELECT * from member;";
+$result_check=$conn->query($sql_check);
+
+if($issueBy =="Student")
+{
+    $sql_m="SELECT * from student;";
 }
-elseif($issueBy =="Faculty"){
-    $sql_m="SELECT * from faculty where Member_ID = '$m'";
+else if($issueBy =="Faculty")
+{
+    $sql_m="SELECT * from faculty;";
 }
 $result_b = $conn->query($sql_b);
 $result_m = $conn->query($sql_m);
 
-if($result_b && $result_m){
-    while($row = $result_b->fetch_assoc()){
-        if($row["Status"] =="Available"){
-            $Available = true;
-        }
-    }
-    if($issueBy =="Student"){
-    while($row = $result_m->fetch_assoc()){
-        if($row["Student_Book1"]==null){
-            $BookNoIssue = 1;
-        }
-        else if($row["Student_Book2"]==null){
-            $BookNoIssue = 2;
-        }
-        else if($row["Student_Book3"]==null){
-            $BookNoIssue = 3;
-        }
-        else{
-            //issueing book limit reached
-            $Available = false;
-        }
-    }  
-    }
-    if($issueBy =="Faculty"){
-        while($row = $result_m->fetch_assoc()){
-            if($row["Faculty_Book1"]==null){
-                $BookNoIssue = 1;
-            }
-            else if($row["Faculty_Book2"]==null){
-                $BookNoIssue = 2;
-            }
-            else if($row["Faculty_Book3"]==null){
-                $BookNoIssue = 3;
-            }
-            else if($row["Faculty_Book4"]==null){
-                $BookNoIssue = 4;
-            }
-            else if($row["Faculty_Book5"]==null){
-                $BookNoIssue = 5;
-            }
-            else{
-                //issueing book limit reached
-                $Available= false;
+function bookcheck($x,$y)
+{
+    if($x)
+    {
+        while($row=$x->fetch_assoc())
+        {
+            if($row["Book_No"] == $y)
+            {
+                return true;
             }
         }
     }
-    if($Available){
-        $sql_ir="INSERT INTO issue_return (Issue_by,Issue_Bookno,Issue_Date)
-        values ('$m','$b','$m','$doi');";
-        $resultIssue=$conn->query($sql_ir);
-        if($resulIssue){
-            if($issueBy =="Student"){
-                $slot ="Student_Book".$BookNoIssue; 
-                $sql_UpdateS="UPDATE student set $slot=$b where Member_ID = '$m';";  
-                $update_student = $conn->query($sql_UpdateS);
-                $sql_Update = "UPDATE books set Status='$m' where Book_No = $b;";
-                $update_book = $conn->query($sql_Update);
+    return false;
+}
+
+function membercheck($x,$y)
+{
+    if($x)
+    {
+        while($row=$x->fetch_assoc())
+        {
+            if($row["Member_ID"] == $y)
+            {
+                return true;
             }
-            else if($issueBy == "Faculty"){
-                $slot ="Faculty_Book".$BookNoIssue; 
-                $sql_UpdateF="UPDATE faculty set $slot=$b where Member_ID = '$m';";  
-                $update_faculty = $conn->query($sql_UpdateF);
-                $sql_Update = "UPDATE books set Status='$m' where Book_No = $b;";
-                $update_book = $conn->query($sql_Update);
-            }
-            echo"Book issued by $m successfully";
         }
+    }
+    return false;
+}
+
+$checkedb=bookcheck($result_b,$b);
+$checkedm=membercheck($result_check,$m);
+
+if($checkedb)
+{
+    if($checkedm)
+    {
+        if($result_b && $result_m)
+        {
+            while($row = $result_b->fetch_assoc())
+            {
+                if($row["Status"] =="Available" && $row["Book_No"] == $b)
+                {
+                    $Available = true;
+                }
+            }
+            if($issueBy =="Student")
+            {
+                while($row = $result_m->fetch_assoc())
+                {
+                    if($row["Student_Book1"]==null)
+                    {
+                        $BookNoIssue = 1;
+                    }
+                    else if($row["Student_Book2"]==null)
+                    {
+                        $BookNoIssue = 2;
+                    }
+                    else if($row["Student_Book3"]==null)
+                    {
+                        $BookNoIssue = 3;
+                    }
+                    else
+                    {
+                        echo "Book issue limit reached for $m";
+                        $Available = false;
+                    }
+                }  
+            }
+            if($issueBy =="Faculty")
+            {
+                while($row = $result_m->fetch_assoc())
+                {
+                    if($row["Faculty_Book1"]==null)
+                    {
+                        $BookNoIssue = 1;
+                    }
+                    else if($row["Faculty_Book2"]==null)
+                    {
+                        $BookNoIssue = 2;
+                    }
+                    else if($row["Faculty_Book3"]==null)
+                    {
+                        $BookNoIssue = 3;
+                    }
+                    else if($row["Faculty_Book4"]==null)
+                    {
+                        $BookNoIssue = 4;
+                    }
+                    else if($row["Faculty_Book5"]==null)
+                    {
+                        $BookNoIssue = 5;
+                    }
+                    else
+                    {
+                        echo "Book issue limit reached for $m";
+                        $Available= false;
+                    }
+                }
+            }
+            if($Available)
+            {
+                $sql_ir="INSERT INTO issue_return (Issue_By,Issue_Bookno,Issue_Date)
+                values ('$m','$b','$doi');";
+                $resultIssue=$conn->query($sql_ir);
+                if($resultIssue)
+                {
+                    if($issueBy =="Student")
+                    {
+                        $slot ="Student_Book".$BookNoIssue; 
+                        $sql_UpdateS="UPDATE student set $slot=$b where Member_ID = '$m';";  
+                        $update_student = $conn->query($sql_UpdateS);
+                        $sql_Update = "UPDATE books set Status='$m' where Book_No = $b;";
+                        $update_book = $conn->query($sql_Update);
+                        if($update_book) echo"Book issued by $m successfully";
+                        else echo $conn->error;
+                    }
+                    else if($issueBy == "Faculty")
+                    {
+                        $slot ="Faculty_Book".$BookNoIssue; 
+                        $sql_UpdateF="UPDATE faculty set $slot=$b where Member_ID = '$m';";  
+                        $update_faculty = $conn->query($sql_UpdateF);
+                        $sql_Update = "UPDATE books set Status='$m' where Book_No = $b;";
+                        $update_book = $conn->query($sql_Update);
+                        if($update_book) echo"Book issued by $m successfully";
+                        else echo $conn->error;
+                    }
+                }
+                else
+                {
+                    echo $conn->error;
+                }
+            }
+            else
+            {
+                echo "Book $b not Available!!!";
+            }
+        }
+        else
+        {
+            echo $conn->error;
+        }
+    }
+    else
+    {
+        echo "Member $m not found!!!";
     }
 }
 else
 {
-    echo "$conn->error";
+    echo "Book $b not found!!!";
 }
 
 ?>
