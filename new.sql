0efc21e6bc364e8bb0d20068fab2b671c25a7365
Kartikey2011yadav <nykay10968@gmail.com>
2023-08-07 09:12:33 +0530

    sql update

--- new.sql
+++ new.sql
@@ -49,14 +49,15 @@ INSERT INTO `admin` (`Username`, `Password`) VALUES
 
 DROP TABLE IF EXISTS `books`;
 CREATE TABLE IF NOT EXISTS `books` (
-  `Book_No` int NOT NULL,
-  `Author1` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Book_No` varchar(10) NOT NULL,
+  `Author1` varchar(50) NOT NULL,
   `Author2` varchar(50) DEFAULT NULL,
   `Author3` varchar(50) DEFAULT NULL,
   `Title` varchar(30) NOT NULL,
   `Edition` varchar(15) NOT NULL,
   `Publisher` varchar(50) NOT NULL,
-  `Total_Pages` int NOT NULL,
+  `Cl_No` float NOT NULL,
+  `Total_Pages` int DEFAULT NULL,
   `Cost` int DEFAULT NULL,
   `Supplier` varchar(50) DEFAULT NULL,
   `Bill_No` varchar(20) DEFAULT NULL,
@@ -68,23 +69,23 @@ CREATE TABLE IF NOT EXISTS `books` (
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`Book_No`, `Author1`, `Author2`, `Author3`, `Title`, `Edition`, `Publisher`, `Total_Pages`, `Cost`, `Supplier`, `Bill_No`, `Status`) VALUES
-(12, 'adahah', '', '', 'agag', 'ahhaha', 'adhahah', 1234, 123, 'jajaja', 'adhahah', 'IT-2k21-01'),
-(13, 'aeajejaj', '', '', 'agag', 'ahhaha', 'aejeja', 1234, 123, 'aejajaja', 'aehaeaj', 'IT-2k21-12'),
-(14, 'ahahaha', 'ahaha', 'fadaha', 'gagaga', 'ahahah', 'ahahaha', 1234, 123, 'daaah', 'ahahajaj', 'IT-2k21-13'),
-(15, 'ahahaha', 'ahaha', 'fadaha', 'gaga', 'aha', 'ahahaha', 1234, 123, 'daaah', 'ahahajaj', 'IT-2k21-14'),
-(16, 'adhah', 'afjak', 'kajajaa', 'gjswjsaj', 'ajajaj', 'adjaja', 123, 1234, 'rjajaj', 'ahajaja', 'IT-2k21-15'),
-(17, 'adhah', 'afjak', 'kajajaa', 'gjswjsaj', 'ajajaj', 'adjaja', 123, 1234, 'rjajaj', 'ahajaja', 'IT-2k21-16'),
-(18, 'adhah', 'afjak', 'kajajaa', 'gjswjsaj', 'ajajaj', 'adjaja', 123, 1234, 'rjajaj', 'ahajaja', 'IT-2k21-17'),
-(19, 'adhah', 'afjak', 'kajajaa', 'gjswjsaj', 'ajajaj', 'adjaja', 123, 1234, 'rjajaj', 'ahajaja', 'IT-2k21-18'),
-(20, 'adhah', 'afjak', 'kajajaa', 'gjswjsaj', 'ajajaj', 'adjaja', 123, 1234, 'rjajaj', 'ahajaja', 'IT-2k21-19'),
-(21, 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj', 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-20'),
-(22, 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj', 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-21'),
-(23, 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj', 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-22'),
-(24, 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj', 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-23'),
-(25, 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj', 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-24'),
-(26, 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj', 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-25'),
-(27, 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj', 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-01');
+INSERT INTO `books` (`Book_No`, `Author1`, `Author2`, `Author3`, `Title`, `Edition`, `Publisher`,`Cl_No`, `Total_Pages`, `Cost`, `Supplier`, `Bill_No`, `Status`) VALUES
+('12', 'adahah', '', '', 'agag', 'ahhaha', 'adhahah',69.69, 1234, 123, 'jajaja', 'adhahah', 'IT-2k21-01'),
+('13', 'aeajejaj', '', '', 'agag', 'ahhaha', 'aejeja',69.69, 1234, 123, 'aejajaja', 'aehaeaj', 'IT-2k21-12'),
+('14', 'ahahaha', 'ahaha', 'fadaha', 'gagaga', 'ahahah', 'ahahaha',69.69, 1234, 123, 'daaah', 'ahahajaj', 'IT-2k21-13'),
+('15', 'ahahaha', 'ahaha', 'fadaha', 'gaga', 'aha', 'ahahaha',69.69, 1234, 123, 'daaah', 'ahahajaj', 'IT-2k21-14'),
+('16', 'adhah', 'afjak', 'kajajaa', 'gjswjsaj', 'ajajaj', 'adjaja',69.69, 123, 1234, 'rjajaj', 'ahajaja', 'IT-2k21-15'),
+('17', 'adhah', 'afjak', 'kajajaa', 'gjswjsaj', 'ajajaj', 'adjaja',69.69, 123, 1234, 'rjajaj', 'ahajaja', 'IT-2k21-16'),
+('18', 'adhah', 'afjak', 'kajajaa', 'gjswjsaj', 'ajajaj', 'adjaja',69.69, 123, 1234, 'rjajaj', 'ahajaja', 'IT-2k21-17'),
+('19', 'adhah', 'afjak', 'kajajaa', 'gjswjsaj', 'ajajaj', 'adjaja',69.69, 123, 1234, 'rjajaj', 'ahajaja', 'IT-2k21-18'),
+('20', 'adhah', 'afjak', 'kajajaa', 'gjswjsaj', 'ajajaj', 'adjaja',69.69, 123, 1234, 'rjajaj', 'ahajaja', 'IT-2k21-19'),
+('21', 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj',69.69, 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-20'),
+('22', 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj',69.69, 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-21'),
+('23', 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj',69.69, 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-22'),
+('24', 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj',69.69, 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-23'),
+('25', 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj',69.69, 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-24'),
+('26', 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj',69.69, 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-25'),
+('27', 'arjajaeaja', '', '', 'adhajaj', 'arjajaj', 'ajajaj',69.69, 1234, 234, 'aejaja', 'ajajkaj', 'IT-2k21-01');
 
 -- --------------------------------------------------------
 
@@ -125,24 +126,24 @@ CREATE TABLE IF NOT EXISTS `issue_return` (
 --
 
 INSERT INTO `issue_return` (`Issue_No`, `Issue_By`, `Member_Type`, `Issue_Bookno`, `Issue_Date`, `Return_Date`) VALUES
-(1, '10', 'Student', 12, '2023-06-01', '2023-06-18'),
-(2, 'IT-2k21-11', 'Student', 12, '2023-06-18', '2023-06-18'),
-(3, 'IT-2k21-12', 'Student', 13, '2023-06-18', NULL),
-(4, 'IT-2k21-13', 'Student', 14, '2023-06-18', NULL),
-(5, 'IT-2k21-14', 'Student', 15, '2023-06-18', NULL),
-(6, 'IT-2k21-15', 'Student', 16, '2023-06-18', NULL),
-(7, 'IT-2k21-16', 'Student', 17, '2023-06-18', NULL),
-(8, 'IT-2k21-17', 'Student', 18, '2023-06-18', NULL),
-(9, 'IT-2k21-18', 'Student', 19, '2023-06-18', NULL),
-(10, 'IT-2k21-19', 'Student', 20, '2023-06-18', NULL),
-(11, 'IT-2k21-20', 'Student', 21, '2023-06-18', NULL),
-(12, 'IT-2k21-21', 'Student', 22, '2023-06-18', NULL),
-(13, 'IT-2k21-22', 'Student', 23, '2023-06-18', NULL),
-(14, 'IT-2k21-23', 'Student', 24, '2023-06-18', NULL),
-(15, 'IT-2k21-24', 'Student', 25, '2023-06-18', NULL),
-(16, 'IT-2k21-25', 'Student', 26, '2023-06-18', NULL),
-(17, 'IT-2k21-01', 'Student', 27, '2023-06-18', NULL),
-(18, 'IT-2k21-01', 'Student', 12, '2023-06-18', NULL);
+(1, '10', 'Student', '12', '2023-06-01', '2023-06-18'),
+(2, 'IT-2k21-11', 'Student', '12', '2023-06-18', '2023-06-18'),
+(3, 'IT-2k21-12', 'Student', '13', '2023-06-18', NULL),
+(4, 'IT-2k21-13', 'Student', '14', '2023-06-18', NULL),
+(5, 'IT-2k21-14', 'Student', '15', '2023-06-18', NULL),
+(6, 'IT-2k21-15', 'Student', '16', '2023-06-18', NULL),
+(7, 'IT-2k21-16', 'Student', '17', '2023-06-18', NULL),
+(8, 'IT-2k21-17', 'Student', '18', '2023-06-18', NULL),
+(9, 'IT-2k21-18', 'Student', '19', '2023-06-18', NULL),
+(10, 'IT-2k21-19', 'Student', '20', '2023-06-18', NULL),
+(11, 'IT-2k21-20', 'Student', '21', '2023-06-18', NULL),
+(12, 'IT-2k21-21', 'Student', '22', '2023-06-18', NULL),
+(13, 'IT-2k21-22', 'Student', '23', '2023-06-18', NULL),
+(14, 'IT-2k21-23', 'Student', '24', '2023-06-18', NULL),
+(15, 'IT-2k21-24', 'Student', '25', '2023-06-18', NULL),
+(16, 'IT-2k21-25', 'Student', '26', '2023-06-18', NULL),
+(17, 'IT-2k21-01', 'Student', '27', '2023-06-18', NULL),
+(18, 'IT-2k21-01', 'Student', '12', '2023-06-18', NULL);
 
 -- --------------------------------------------------------
 
