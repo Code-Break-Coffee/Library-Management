9549ed5d4759d9186d9e6fe1f795305b5243cfea
Kartikey2011yadav <nykay10968@gmail.com>
2023-08-08 13:59:22 +0530

    lol

--- test1.php
+++ test1.php
@@ -24,22 +24,22 @@ for($i=0;$i<25;$i++)
         else echo $conn->error;
     }
 }
-// $member=1;
-// for($i=0;$i<75;$i++)
-// {
-//     if($i<9)
-//     {
-//         $sql1="INSERT into student(Student_Rollno,Student_Name,Student_Course,Student_Enrollmentno) values('IT2k210".($member+$i)."','$name','$course','DE210200".($member+$i)."');";
-//         $result1=$conn->query($sql1);
-//         if($result1) echo "$i record added!!!\n";
-//         else echo $conn->error;
-//     }
-//     else
-//     {
-//         $sql1="INSERT into student(Student_Rollno,Student_Name,Student_Course,Student_Enrollmentno) values('IT2k21".($member+$i)."','$name','$course','DE21020".($member+$i)."');";
-//         $result1=$conn->query($sql1);
-//         if($result1) echo "$i record added!!!\n";
-//         else echo $conn->error;
-//     }
-// }
+$member=1;
+for($i=0;$i<75;$i++)
+{
+    if($i<9)
+    {
+        $sql1="INSERT into student(Student_Rollno,Student_Name,Student_Course,Student_Enrollmentno) values('IT2k210".($member+$i)."','$name','$course','DE210200".($member+$i)."');";
+        $result1=$conn->query($sql1);
+        if($result1) echo "$i record added!!!\n";
+        else echo $conn->error;
+    }
+    else
+    {
+        $sql1="INSERT into student(Student_Rollno,Student_Name,Student_Course,Student_Enrollmentno) values('IT2k21".($member+$i)."','$name','$course','DE21020".($member+$i)."');";
+        $result1=$conn->query($sql1);
+        if($result1) echo "$i record added!!!\n";
+        else echo $conn->error;
+    }
+}
 ?>
