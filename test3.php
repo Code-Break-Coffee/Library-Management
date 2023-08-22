0c64ede51cfc96db03607c8df933c78936522489
zakie2003 <121384625+zakie2003@users.noreply.github.com>
2023-08-22 15:53:14 +0530

    test3 correction

--- test3.php
+++ test3.php
@@ -9,7 +9,7 @@ for($i=0;$i<25;$i++)
 {
     if($i<9)
     {
-        $sql1="INSERT into member(Member_ID,MemberType) values('FID-0".($member+$i)."','$type');";
+        $sql1="INSERT into member(Member_ID,MemberType) values('FID0".($member+$i)."','$type');";
         $result1=$conn->query($sql1);
         if($result1) echo "$i record added!!!\n";
         else{
@@ -18,7 +18,7 @@ for($i=0;$i<25;$i++)
     }
     else
     {
-        $sql1="INSERT into member(Member_ID,MemberType) values('FID-".($member+$i)."','$type');";
+        $sql1="INSERT into member(Member_ID,MemberType) values('FID".($member+$i)."','$type');";
         $result1=$conn->query($sql1);
         if($result1) echo "$i record added!!!\n";
         else echo $conn->error;
@@ -29,14 +29,14 @@ for($i=0;$i<75;$i++)
 {
     if($i<9)
     {
-        $sql1="INSERT into faculty(Faculty_ID,Faculty_Name,Faculty_Type,Faculty_Fatherorhusband) values('FID-0".($member+$i)."','$name','$course','NA');";
+        $sql1="INSERT into faculty(Faculty_ID,Faculty_Name,Faculty_Type,Faculty_Fatherorhusband) values('FID0".($member+$i)."','$name','$course','NA');";
         $result1=$conn->query($sql1);
         if($result1) echo "$i record added!!!\n";
         else echo $conn->error;
     }
     else
     {
-        $sql1="INSERT into faculty(Faculty_ID,Faculty_Name,Faculty_Type,Faculty_Fatherorhusband) values('FID-".($member+$i)."','$name','$course','NA');";
+        $sql1="INSERT into faculty(Faculty_ID,Faculty_Name,Faculty_Type,Faculty_Fatherorhusband) values('FID".($member+$i)."','$name','$course','NA');";
         $result1=$conn->query($sql1);
         if($result1) echo "$i record added!!!\n";
         else echo $conn->error;
