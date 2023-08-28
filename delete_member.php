8dc8578c60dcb05cc423bfe98804ab6eff67e60e
Kartikey2011yadav <nykay10968@gmail.com>
2023-08-28 07:48:33 +0530

    Delete member done

--- delete_member.php
+++ delete_member.php
@@ -40,51 +40,46 @@
 
     if(member_check($stat_check,$member))
     {
-        $stat_check2="SELECT * FROM member where Member_ID='$member';";
+        $mem ="";
+        $stat_check2="SELECT Member_ID FROM member where Member_ID='$member';";
         $r1=$conn->query($stat_check2);
-        while($row=$r1->fetch_assoc())
+        while($row=$r1->fetch_assoc()) $mem =$row["Member_ID"];
+        $stat_issue="SELECT Issue_By FROM issue_return where Issue_By='$member' and Return_Date is NULL;";
+        if(!issue_check($stat_issue,$member))
         {
-            $stat_issue="SELECT Issue_By FROM issue_return where Issue_By='$member' and Return_Date is NULL;";
-            if(!issue_check($stat_issue,$member))
+            if(substr($mem,0,2)==$course)
             {
-                if(substr($row["Member_ID"],0,2)==$course)
+                $result=$conn->query($stat);
+                if($result)
                 {
-                    $result=$conn->query($stat);
-                    if($result)
-                    {
-                        echo "
-                            <div id='dialog_del' style='color:green;' title='✅ Successful'>
-                                <p>Member $member Deleted Succesfully</p>
-                            </div>
-                        "; 
-                    }
-                    else
-                    {
-                        echo "
-                        <div id='dialog_del' style='color:red;' title='❌Error'>
-                            <p>$conn->error</p>
-                        </div>
-                    "; 
-                    }
+                    echo "
+                        <div id='dialog_del' style='color:green;' title='✅ Successful'>
+                            <p>Member $member Deleted Succesfully</p>
+                        </div>"; 
                 }
                 else
                 {
                     echo "
-                    <div id='dialog_del' style='color:red;' title='❌Error'>
-                        <p>Member $member And Course Do Not Match</p>
-                    </div>
-                    ";
+                        <div id='dialog_del' style='color:red;' title='❌Error'>
+                            <p>$conn->error</p>
+                        </div>"; 
                 }
             }
             else
             {
                 echo "
+                    <div id='dialog_del' style='color:red;' title='❌Error'>
+                        <p>Member $member And Course Do Not Match</p>
+                    </div>";
+            }
+        }
+        else
+        {
+            echo "
                 <div id='dialog_del' style='color:red;' title='❌Error'>
                     <p>Member $member has a Issued Book</p>
-                </div>
-                ";
+                </div>";
 
-            }
         }
     }
     else
