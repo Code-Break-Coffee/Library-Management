c29ea504dbd2048d40f20567f62f69adebfd810c
zakie2003 <121384625+zakie2003@users.noreply.github.com>
2023-09-04 13:25:49 +0530

    PR utha

---
+++ delete_user.php
@@ -0,0 +1,7 @@
+<?php
+            echo "
+            <div id='dialog_admin_disp' style='color:green;' title='⚠️Successfull'>
+                <p><center>Deletion successful</center></p>
+            </div>
+            "; 
+?>
