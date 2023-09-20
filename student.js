3c95747b9cea39f60d1689f2f76224d7fdb51ef8
SimpPoseidon <mehrunkart@gmail.com>
2023-09-20 19:10:15 +0530

    student update

--- student.js
+++ student.js
@@ -11,8 +11,10 @@ $(document).ready(function()
             datatype: "text",
             success: function(Response)
             {
+                document.getElementById("formblock").style.transform="translate(-185%,-60%)";
+                document.getElementById("searchtext").style.textShadow="2px 2px aliceblue";
+                document.getElementById("searchtext").style.color="#040D12";
                 document.getElementById("iframeblock").style.display="block";
-                document.getElementById("formblock").style.transform="translate(-180%,-60%)";
                 $("#iframeblock").html(Response);
             }
         });
@@ -54,6 +56,8 @@ function cleared()
     let holder=document.getElementById("book_s");
     document.getElementById("iframeblock").style.display="none";
     document.getElementById("formblock").style.transform="translate(-50%,-60%)";
+    document.getElementById("searchtext").style.textShadow="2px 2px #040d12";
+    document.getElementById("searchtext").style.color="#93b1a6";
     holder.value="";
     document.getElementById("clear2").style.display="none";
 }
