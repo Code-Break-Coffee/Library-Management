ab36ead626c967f49b834c6c149e15a89757bfa7
Kartikey2011yadav <nykay10968@gmail.com>
2023-10-21 07:00:31 +0530

    confirmation update

--- Book_add_excel.php
+++ Book_add_excel.php
@@ -1,17 +1,28 @@
 <?php
 @session_start();
 include "auth.php";
-if(!verification() || $_POST["Access"] != "Main-Book_add_excel" )
-{
-    header("Location: /LibraryManagement/");
-}
-use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 include "dbconnect.php";
-require_once ('vendor/autoload.php');
-
 $BookIndex =0;
 $BookSlots =[]; // Available slots in between
 $error = "";
+$Data_Status = false;
+if(verification() && $_POST["Access"] == "Book_add_excel-Confirmation"){
+    if($Data_Status && strlen($error) == 0 && count($Book_Record)>0){
+        
+    }
+    else{
+        // return error and reload
+    }
+    
+}
+elseif(!verification() || $_POST["Access"] != "Main-Book_add_excel" )
+{
+    header("Location: /LibraryManagement/");
+}
+
+require_once ('vendor/autoload.php');
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+
 
 function Book_check($b,$count){
     include "dbconnect.php";
@@ -44,7 +55,7 @@ function set_BookIndex($count){
         array_push($a, 0);
         $res = [];
         $stage = [];
-
+        
         foreach($a as $i) {
             if(count($stage) > 0 && $i != $stage[count($stage)-1]+1) {
                 if(count($stage) > 1) {
@@ -60,7 +71,7 @@ function set_BookIndex($count){
                 return;
             }
         }
-
+        
         $sql_max_book = "SELECT Book_No from books;";
         $res=$conn->query($sql_max_book);
         $bookno = 0;
@@ -130,7 +141,6 @@ $excelSheet = $spreadSheet->getActiveSheet();
 $spreadSheetAry = $excelSheet->toArray();
 $sheetCount = count($spreadSheetAry);
 $bno =0;
-$Book_Record =[];
 $error;
 $author1;
 $author2;
@@ -144,6 +154,7 @@ $supplier;
 $remark;
 $billno;
 $bookcount;
+$Book_Record =[];
 
 $bookserial =[];
 for($i = 1;$i<$sheetCount; $i++){
@@ -235,68 +246,77 @@ if(sizeof($temp_array) == sizeof($bookserial))
         if(!array_key_exists($bno,$Book_Record))$Book_Record[$bno]= array($author1,$author2,$author3,$title,$edition,$publisher,$cl_no,$total_pages,$cost,$supplier,$remark,$billno,$bookcount);
         
     }
-    // print_r($Book_Record);
-    echo "
-                <div style='width:100%;overflow:auto;height:650px;'><table>
-                <tr>
-                <th>Book No.</th>
-                <th>Author's</th>
-                <th>Title</th>
-                <th>Edition</th>
-                <th>Publisher</th>
-                <th>No of Copies</th>
-                </tr>
-                <tbody>";
-
-            ksort($Book_Record);
-            foreach($Book_Record as $bn=>$b)
-            {
-                echo"
-                <tr>
-                <td>".$bn."</td>
-                <td>".$b[0]." ".$b[1]." ".$b[2]." "."</td>
-                <td>".$b[3]."</td>
-                <td>".$b[4]."</td>
-                <td>".$b[5]."</td>
-                <td>".$b[12]."</td>
-                <td></td>
-                <td></td>
-                </tr>
-                ";
-            }
-
-            echo"
-                </tbody></table>
-                <form id='confirm' method= 'post' action=''>
-                <center>
-                    <button class='btn' style='color:aliceblue; background-color:black;' type='submit' >Confirm</button>
-                </center>
-                </form>
-                <script>
-                $(document).ready(function()
+    if(count($Book_Record)> 0){
+        $Data_Status = true;
+        // print_r($Book_Record);
+        echo "
+                    <div style='width:100%;overflow:auto;height:650px;'><table>
+                    <tr>
+                    <th>Book No.</th>
+                    <th>Author's</th>
+                    <th>Title</th>
+                    <th>Edition</th>
+                    <th>Publisher</th>
+                    <th>No of Copies</th>
+                    </tr>
+                    <tbody>";
+    
+                ksort($Book_Record);
+                foreach($Book_Record as $bn=>$b)
                 {
-                    $('#confirm').submit(function(e)
+                    echo"
+                    <tr>
+                    <td>".$bn."</td>
+                    <td>".$b[0]." ".$b[1]." ".$b[2]." "."</td>
+                    <td>".$b[3]."</td>
+                    <td>".$b[4]."</td>
+                    <td>".$b[5]."</td>
+                    <td>".$b[12]."</td>
+                    <td></td>
+                    <td></td>
+                    </tr>
+                    ";
+                }
+    
+                echo"
+                    </tbody></table>
+                    <form id='confirm' method= 'post' action=''>
+                    <center>
+                        <button class='btn' style='color:aliceblue; background-color:black;' type='submit' >Confirm</button>
+                    </center>
+                    </form>
+                    <script>
+                    $(document).ready(function()
                     {
-                        e.preventDefault();
-                        $.ajax(
+                        $('#confirm').submit(function(e)
                         {
-                            method: 'post',
-                            url: 'Book_add_excel.php',
-                            data: $(this).serialize()+'&Access=Main-Book_add_excel',
-                            datatype: 'text',
-                            success: function(Result)
+                            e.preventDefault();
+                            $.ajax(
                             {
-                                
-                                $( '#dialog_exl_disp' ).dialog( 'destroy');
-                                $('#response_exl_records').html(Result);
-                                $('#dialog_exl_disp').dialog();  
-                            }
-                        });
-                    }); 
-                });
-                </script>
-                </div>
-                ";
+                                method: 'post',
+                                url: 'Book_add_excel.php',
+                                data: {
+                                    'Access': 'Book_add_excel-Confirmation',
+                                    'Status': 'confirm-Insert'
+                                }
+                                datatype: 'text',
+                                success: function(Result)
+                                {
+                                    
+                                    $( '#dialog_exl_disp' ).dialog( 'destroy');
+                                    $('#response_exl_records').html(Result);
+                                    $('#dialog_exl_disp').dialog();  
+                                }
+                            });
+                        }); 
+                    });
+                    </script>
+                    </div>
+                    ";
+    }
+    else{
+        echo "No data found";
+    }
 
 
 }
