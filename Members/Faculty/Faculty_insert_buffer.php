bcd9b453ac2a3d5910fdf0d4dfbf41243ae87a98
Kartikey2011yadav <nykay10968@gmail.com>
2024-01-19 21:15:17 +0530

    faculty insert buffer update

--- Members/Faculty/Faculty_insert_buffer.php
+++ Members/Faculty/Faculty_insert_buffer.php
@@ -1,7 +1,7 @@
 <?php
     include "../../connection/dbconnect.php";
 
-    $stat="select val1,val2,val3,val4 from `insert buffer`;";
+    $stat="select val1,val2,val3 from `insert buffer`;";
     $result=$conn->query($stat);
     
     if($result)
@@ -13,21 +13,21 @@
         
         for($i=0;$i<count($sArray);$i++)
         {
-            $rollno=$sArray[$i][0];
-            $rollno=strtoupper($rollno);
-            $rollno=str_replace("-","",$rollno);
-            $name=$sArray[$i][1];
-            $course=$sArray[$i][2];
-            $enroll=$sArray[$i][3];
-            $stat1="insert into student(Student_Rollno,Student_Name,Student_Course,Student_Enrollmentno) values ('$rollno','$name','$course','$enroll');";
+            $fcaId=$sArray[$i][0];
+            $fcaId=strtoupper($fcaId);
+            $fcaId=str_replace("-","",$fcaId);
+            $fName=$sArray[$i][1];
+            $fType=$sArray[$i][2];
+
+            $stat1="insert into faculty(Faculty_ID,Faculty_Name,Faculty_Type) values ('$fcaId','$fName','$fType');";
             $res=$conn->query($stat1);
             if($res)
             {
-                echo"
-                <div id='dialog_student_excel' style='color:green;' title='Succesfull'>
-                    <p><center>Data Inserted Successfully</center></p>
-                </div>;
-                ";
+                // echo"
+                // <div id='dialog_student_excel' style='color:green;' title='Succesfull'>
+                //     <p><center>Data Inserted Successfully</center></p>
+                // </div>;
+                // ";
                 $sql_delete = "DELETE from `insert buffer`;";
                 $result=$conn->query($sql_delete);
             }
