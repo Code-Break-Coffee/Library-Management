4a5eb68c0f161d5568f097060294a8d5ac23c87b
zakie khan <121384625+zakie2003@users.noreply.github.com>
2024-07-28 20:35:25 +0530

    Roll Back Part -1

--- Members/Student/Student_add_excel.php
+++ Members/Student/Student_add_excel.php
@@ -7,11 +7,8 @@ include $_SERVER['DOCUMENT_ROOT'] . "/LibraryManagement/Auth/auth.php";
 
 if (!verification() || $_POST["Access"] != "Main-Student_add_excel") {
     header("Location: /LibraryManagement/");
+    exit();
 }
-
-
-// $sql_delete = "DELETE from `insert buffer`;";
-// $result=$conn->query($sql_delete);
 
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
@@ -22,65 +19,43 @@ $excelSheet = $spreadSheet->getActiveSheet();
 $spreadSheetAry = $excelSheet->toArray();
 $sheetCount = count($spreadSheetAry);
 
+$already_exist = 0;
 
-$sql_delete = "DELETE from `insert buffer`;";
-$result=$conn->query($sql_delete);
-$already_exist=0;
-
-function check($sArray,$sCount)
+function check($sArray, $sCount)
 {
-    include "../../connection/dbconnect.php";
-    for($i=1;$i<$sCount;$i++)
-    {
-        $rollno=$sArray[$i][0];
-        $rollno=strtoupper($rollno);
-        $rollno=str_replace("-","",$rollno);
-        $rollno=str_replace("_","",$rollno);
-        $rollno=preg_replace('/[^A-Za-z0-9\-]/', '', $rollno);
-        $name=$sArray[$i][1];
-        $course=$sArray[$i][2];
-        $enroll=$sArray[$i][3];
-        if(empty($rollno) || empty($name) || empty($course) || empty($enroll))
-        {
-            $index =$i+1;
-            echo
-            "
+    global $conn;
+    for ($i = 1; $i < $sCount; $i++) {
+        $rollno = strtoupper(preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(["-", "_"], "", $sArray[$i][0])));
+        $name = $sArray[$i][1];
+        $course = $sArray[$i][2];
+        $enroll = $sArray[$i][3];
+        
+        if (empty($rollno) || empty($name) || empty($course) || empty($enroll)) {
+            $index = $i + 1;
+            echo "
                 <div id='dialog_exl_disp_student' style='color:red;' title='❌Not Allowed'>
                     <p><center>An Excel Field is Empty at Index $index!!!</center></p>
                 </div>
             ";
             return false;
         }
-        $sql_check="SELECT Student_Rollno from student where Student_Rollno = '$rollno';";
-        $result_check=$conn->query($sql_check);
-        if($result_check)
-        {
-            if(mysqli_num_rows($result_check)>0)
-            {
-                echo
-                "
+
+        $sql_check = "SELECT Student_Rollno FROM student WHERE Student_Rollno = '$rollno';";
+        $result_check = $conn->query($sql_check);
+        
+        if ($result_check) {
+            if (mysqli_num_rows($result_check) > 0) {
+                echo "
                     <div id='dialog_exl_disp_student' style='color:red;' title='❌Error'>
-                        <p>
-                            <center>
-                                '$rollno' is already present as a student. Please check the excel once!!!
-                            </center>
-                        </p>
+                        <p><center>'$rollno' is already present as a student. Please check the excel once!!!</center></p>
                     </div>
                 ";
-                
                 return false;
             }
-        }
-        else
-        {
-            echo 
-            "
+        } else {
+            echo "
                 <div id='dialog_exl_disp_student' style='color:red;' title='❌Error'>
-                    <p>
-                        <center>
-                            $conn->error
-                        </center>
-                    </p>
+                    <p><center>" . $conn->error . "</center></p>
                 </div>
             ";
             return false;
@@ -89,43 +64,40 @@ function check($sArray,$sCount)
     return true;
 }
 
-if(check($spreadSheetAry,$sheetCount))
-{
-    $flag=0;
-    for($i=1;$i<$sheetCount;$i++)
-    {
-        $rollno=$spreadSheetAry[$i][0];
-        $rollno=strtoupper($rollno);
-        $rollno=str_replace("-","",$rollno);
-        $rollno=preg_replace('/[^A-Za-z0-9\-]/', '', $rollno);
-        $name=$spreadSheetAry[$i][1];
-        $course=$spreadSheetAry[$i][2];
-        $enroll=$spreadSheetAry[$i][3];
-        $sql="INSERT into `insert buffer`(id,val1,val2,val3,val4)
-        values($i,'$rollno','$name','$course','$enroll');";
-       
-        $result=$conn->query($sql); 
-        if(!$result)
-        {
-            echo
-            "
-                <div id='dialog_exl_disp_student' style='color:red;' title='❌Error'>
-                    <p><center>There was en error in the excel data format in the row of Roll No '$rollno'.</center></p>
-                </div>
-            ";
-            $flag=1;
-            break;
-        }  
+try {
+    // Start transaction
+    $conn->begin_transaction();
+
+    // Delete existing data from the buffer table
+    $sql_delete = "DELETE FROM `insert buffer`;";
+    $result = $conn->query($sql_delete);
+
+    if (!$result) {
+        throw new Exception("Failed to delete from buffer: " . $conn->error);
     }
-    if($flag==0)
-    {
-        if($sheetCount>1)
-        {
+
+    if (check($spreadSheetAry, $sheetCount)) {
+        $flag = 0;
+        for ($i = 1; $i < $sheetCount; $i++) {
+            $rollno = strtoupper(preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(["-", "_"], "", $spreadSheetAry[$i][0])));
+            $name = $spreadSheetAry[$i][1];
+            $course = $spreadSheetAry[$i][2];
+            $enroll = $spreadSheetAry[$i][3];
+
+            $sql = "INSERT INTO `insert buffer` (id, val1, val2, val3, val4) VALUES ($i, '$rollno', '$name', '$course', '$enroll');";
+            $result = $conn->query($sql);
+
+            if (!$result) {
+                throw new Exception("Failed to insert into buffer: " . $conn->error);
+            }
+        }
+
+        if ($flag == 0 && $sheetCount > 1) {
             echo "
             <style>
             .gola::-webkit-scrollbar {
                 display: none; 
-              }
+            }
             </style>   
             <center>     
             <div style='border-radius:50%;width:700px;height:700px;'>
@@ -154,28 +126,24 @@ if(check($spreadSheetAry,$sheetCount))
             </tr>
             <tbody>";
 
-        // ksort($Book_Record);
-        $ignore=0;
-        foreach($spreadSheetAry as $bn=>$b)
-        {
-            if($ignore==0)
-            {
-                $ignore=$ignore+1;
-            }
-            else
-            {
-                echo"
+            $ignore = 0;
+            foreach ($spreadSheetAry as $bn => $b) {
+                if ($ignore == 0) {
+                    $ignore++;
+                } else {
+                    echo "
                 <tr>
-                <td>".$bn."</td>
-                <td>".preg_replace('/[^A-Za-z0-9\-]/', '', $b[0])."</td>
-                <td>".$b[1]."</td>
-                <td>".$b[2]."</td>
-                <td>".$b[3]."</td>
+                <td>" . $bn . "</td>
+                <td>" . preg_replace('/[^A-Za-z0-9\-]/', '', $b[0]) . "</td>
+                <td>" . $b[1] . "</td>
+                <td>" . $b[2] . "</td>
+                <td>" . $b[3] . "</td>
                 </tr>
                 ";
+                }
             }
-        }
-        echo"
+
+            echo "
             </tbody>
             </table>
             </div>
@@ -191,69 +159,56 @@ if(check($spreadSheetAry,$sheetCount))
             <div style='font-weight: bold;' id='response_student_excel'></div>
             </center>
             <script>
-            $(document).ready(function()
-            {
-                
-                $('#buff_back').click(function(e)
-                {
+            $(document).ready(function() {
+                $('#buff_back').click(function(e) {
                     $.ajax({
-
                         method: 'post',
                         url: './Members/Student/Student_empty_buffer.php',
-                        error: function()
-                        {
+                        error: function() {
                             alert('Some Error Occurred!!!');
                         }
                     })
-                })
+                });
                 
-                $('#std_buffer').click(function(e)
-                {
-                    
+                $('#std_buffer').click(function(e) {
                     $.ajax({
-                        
                         method: 'post',
                         url: './Members/Student/Student_insert_buffer.php',
-                        data:'hi',
-                        datatype:'text',
-                        error: function()
-                        {
+                        data: 'hi',
+                        datatype: 'text',
+                        error: function() {
                             alert('Some Error Occurred!!!');
                         },
-                        success: function(Result)
-                        {
-                            
+                        success: function(Result) {
                             $('#dialog_student_excel').dialog('destroy');
                             $('#response_student_excel').html(Result);
                             $('#dialog_student_excel').dialog();
-                            
                         }
                     }) 
-                })
-            })
-            
+                });
+            });
             </script>
             ";
-    }
-    else
-    {
-        echo"
-        <div id='dialog_exl_disp_student' style='color:red;' title='❌Not Allowed'>
-            <p><center>Excel File is Empty!!!</center></p>
-        </div>
-        ";
+        } else {
+            echo "
+            <div id='dialog_exl_disp_student' style='color:red;' title='❌Not Allowed'>
+                <p><center>Excel File is Empty!!!</center></p>
+            </div>
+            ";
+        }
+    } else {
+        throw new Exception("Data validation failed.");
     }
 
-    }
-    if($flag == 1)
-    {
-        $sql_delete = "DELETE from `insert buffer`;";
-        $result=$conn->query($sql_delete);
-    }
-}
-else
-{
-    $sql_delete = "DELETE from `insert buffer`;";
-    $result=$conn->query($sql_delete);
+    // Commit the transaction
+    $conn->commit();
+} catch (Exception $e) {
+    // Roll back the transaction if any error occurs
+    $conn->rollback();
+    echo "
+        <div id='dialog_exl_disp_student' style='color:red;' title='❌Transaction Failed'>
+            <p><center>Transaction failed: " . $e->getMessage() . "</center></p>
+        </div>
+    ";
 }
 ?>
